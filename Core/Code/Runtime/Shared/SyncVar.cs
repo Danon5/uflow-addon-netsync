1ee93ed749a316add59732f3621d95c286be6ccd
Danon5 <danonfive@gmail.com>
2023-06-23 19:44:17 -0700

    - Fixed Root trying to call OnDrawGizmos in edit mode

--- Core/Code/Runtime/Shared/SyncVar.cs
+++ Core/Code/Runtime/Shared/SyncVar.cs
@@ -1,17 +1,26 @@
-﻿namespace UFlow.Addon.NetSync.Runtime {
-    public struct SyncVar<T> {
-        internal T value;
+﻿using System;
+using MemoryPack;
 
+namespace UFlow.Addon.NetSync.Runtime {
+    [MemoryPackable]
+    public unsafe partial struct SyncVar<T> where T : unmanaged {
+        internal T value;
+        private static readonly int s_size = sizeof(T);
+        
         public SyncVar(in T value) {
             this.value = value;
         }
 
         public static implicit operator T(in SyncVar<T> sv) => sv.value;
         public static implicit operator SyncVar<T>(in T v) => new(v);
-        public static bool operator ==(in SyncVar<T> a, in SyncVar<T> b) => a.Equals(b);
-        public static bool operator !=(in SyncVar<T> a, in SyncVar<T> b) => !a.Equals(b);
-        public static bool operator ==(in T a, in SyncVar<T> b) => a != null && a.Equals(b.value);
-        public static bool operator !=(in T a, in SyncVar<T> b) => a != null && !a.Equals(b.value);
+        public static bool operator ==(SyncVar<T> a, SyncVar<T> b) =>
+            new ReadOnlySpan<byte>(&a.value, s_size).SequenceEqual(new ReadOnlySpan<byte>(&b.value, s_size));
+        public static bool operator !=(SyncVar<T> a, SyncVar<T> b) =>
+            new ReadOnlySpan<byte>(&a.value, s_size).SequenceEqual(new ReadOnlySpan<byte>(&b.value, s_size)) == false;
+        public static bool operator ==(T a, SyncVar<T> b) =>
+            new ReadOnlySpan<byte>(&a, s_size).SequenceEqual(new ReadOnlySpan<byte>(&b.value, s_size));
+        public static bool operator !=(T a, SyncVar<T> b) =>
+            new ReadOnlySpan<byte>(&a, s_size).SequenceEqual(new ReadOnlySpan<byte>(&b.value, s_size)) == false;
         public override string ToString() => value.ToString();
         public override int GetHashCode() => value.GetHashCode();
         public override bool Equals(object obj) => obj != null && this == (SyncVar<T>)obj;
