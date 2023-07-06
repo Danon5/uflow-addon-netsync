d69767039ca60f0754394f4e6420a50d452e66b9
Danon5 <danonfive@gmail.com>
2023-07-06 10:43:44 -0700

    - Fixed title GUI style in AddonSettingsCache

--- Core/Code/Runtime/Shared/Settings/NetSyncAddonSettings.cs
+++ Core/Code/Runtime/Shared/Settings/NetSyncAddonSettings.cs
@@ -1,6 +1,10 @@
 ﻿using UFlow.Core.Runtime;
+using UnityEngine;
 
 namespace UFlow.Addon.NetSync.Core.Runtime {
+    [CreateAssetMenu(
+        fileName = nameof(NetSyncAddonSettings),
+        menuName = MENU_NAME + nameof(NetSyncAddonSettings))]
     public sealed class NetSyncAddonSettings : BaseAddonSettings {
         public override string AddonName => "NetSync";
     }
