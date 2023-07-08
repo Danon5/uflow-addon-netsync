f6aef15e5dfdc5b69a77e10c59ccdd46d4b9ee8f
Danon5 <danonfive@gmail.com>
2023-07-08 13:54:13 -0700

    - Removed MemoryPack plugni

--- Core/Code/Runtime/Shared/Attributes/NetStateAttribute.cs
+++ Core/Code/Runtime/Shared/Attributes/NetStateAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
 namespace UFlow.Addon.NetSync.Core.Runtime.Attributes {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Property | AttributeTargets.Field)]
     public sealed class NetStateAttribute : Attribute { }
 }
