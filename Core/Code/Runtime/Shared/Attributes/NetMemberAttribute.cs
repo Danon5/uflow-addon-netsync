355580c0ad78b530d2d0dbf3b648d51411d71232
Danon5 <danonfive@gmail.com>
2023-07-09 15:03:19 -0700

    - Progress on serialization addon, added ByteBuffer

---
+++ Core/Code/Runtime/Shared/Attributes/NetMemberAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace UFlow.Addon.NetSync.Core.Runtime.Attributes {
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public sealed class NetMemberAttribute : Attribute {
+        internal readonly int order;
+
+        public NetMemberAttribute([CallerLineNumber] int order = default) {
+            this.order = order;
+        }
+    }
+}
