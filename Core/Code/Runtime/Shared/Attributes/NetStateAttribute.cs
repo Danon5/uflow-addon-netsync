9b74c6d1af25be9d99cd105074c303068c28825d
Danon5 <danonfive@gmail.com>
2023-07-11 16:10:50 -0700

    - Moved NetState / NetMember to serialization for convenience

--- Core/Code/Runtime/Shared/Attributes/NetStateAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace UFlow.Addon.NetSync.Core.Runtime.Attributes {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Property | AttributeTargets.Field)]
-    public sealed class NetStateAttribute : Attribute { }
-}
