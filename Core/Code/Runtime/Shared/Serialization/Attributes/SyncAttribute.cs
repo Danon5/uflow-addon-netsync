0d3b8fac4f21b2cb8464d293f014219dec3d0598
Danon5 <danonfive@gmail.com>
2023-07-13 16:24:19 -0700

    - Added Serialization / SaveState addons

---
+++ Core/Code/Runtime/Shared/Serialization/Attributes/SyncAttribute.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace UFlow.Addon.NetSync.Core.Runtime {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Property | AttributeTargets.Field)]
+    public sealed class SyncAttribute : Attribute { }
+}
