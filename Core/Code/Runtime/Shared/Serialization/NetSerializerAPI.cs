c68208becffe03004d5fd3772a857aa0aab90501
Danon5 <danonfive@gmail.com>
2023-08-08 21:03:54 -0700

    - Renamed serialization API classes

--- Core/Code/Runtime/Shared/Serialization/NetSerializerAPI.cs
+++ Core/Code/Runtime/Shared/Serialization/NetSerializerAPI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Reflection;
 
 namespace UFlow.Addon.NetSync.Core.Runtime {
-    public static class NetSerialization {
+    public static class NetSerializerAPI {
         private static readonly Dictionary<Type, MethodInfo> s_serializeRpcCache = new();
     }
 }
