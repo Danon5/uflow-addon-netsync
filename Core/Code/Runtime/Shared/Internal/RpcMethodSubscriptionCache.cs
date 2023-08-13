e81ff14aa449b4ed5e6bef20bedb578d6fc2d5a1
Danon5 <danonfive@gmail.com>
2023-08-13 16:16:19 -0700

    - Created RPC method cache

---
+++ Core/Code/Runtime/Shared/Internal/RpcMethodSubscriptionCache.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using UFlow.Addon.ECS.Core.Runtime;
+
+namespace UFlow.Addon.NetSync.Core.Runtime {
+    internal sealed class RpcMethodSubscriptionCache<T> where T : INetRpc {
+        private static RpcMethodDelegate<T> s_methods;
+
+        static RpcMethodSubscriptionCache() => ExternalEngineEvents.clearStaticCachesEvent += ClearStaticCaches;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void RegisterMethod(in RpcMethodDelegate<T> method) => s_methods += method;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void InvokeMethods(in T rpc) => s_methods.Invoke(rpc);
+        
+        private static void ClearStaticCaches() => s_methods = null;
+    }
+}
--- Core/Code/Runtime/Shared/Internal/RpcTypeMap.cs
+++ Core/Code/Runtime/Shared/Internal/RpcTypeMap.cs
@@ -1,30 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using UFlow.Addon.Serialization.Core.Runtime;
 using UFlow.Core.Runtime;
 
 namespace UFlow.Addon.NetSync.Core.Runtime {
-    internal sealed class RpcTypeMap {
-        private static readonly Dictionary<Type, ulong> s_typeToHash = new();
-        private static readonly Dictionary<ulong, Type> s_hashToType = new();
-        private static bool s_initialized;
+    public sealed class RpcTypeMap {
+        private static readonly Dictionary<Type, ushort> s_typeToHash = new();
+        private static readonly Dictionary<ushort, Type> s_hashToType = new();
 
         static RpcTypeMap() {
+            var initialHandshakeRpcType = typeof(InitialHandshakeRpc);
+            s_typeToHash.Add(initialHandshakeRpcType, 0);
+            s_hashToType.Add(0, initialHandshakeRpcType);
+            ushort nextId = 1;
             foreach (var type in UFlowUtils.Reflection.GetInheritors<INetRpc>()) {
-                var hash = SerializationAPI.CalculateHash(type.ToString());
+                if (type == initialHandshakeRpcType) continue;
+                var hash = nextId++;
                 s_typeToHash[type] = hash;
                 s_hashToType[hash] = type;
             }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong GetHash(in Type type) => s_typeToHash[type];
+        public static ushort GetHash(in Type type) => s_typeToHash[type];
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Type GetType(ulong hash) => s_hashToType[hash];
+        public static Type GetType(ushort hash) => s_hashToType[hash];
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerable<Type> GetRegisteredTypesEnumerable() => s_typeToHash.Keys;
+
+        internal static IEnumerable<(Type, ushort)> GetTypeHashPairsEnumerable() {
+            foreach (var (type, hash) in s_typeToHash)
+                yield return (type, hash);
+        }
     }
 }
