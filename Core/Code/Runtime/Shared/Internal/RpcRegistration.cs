67179d84ea3b9b85fadba565a120651fc1002156
Danon5 <danonfive@gmail.com>
2023-08-13 22:40:00 -0700

    added common exclusion namespaces + exclusion params to reflection utilities

---
+++ Core/Code/Runtime/Shared/Internal/RpcRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using UFlow.Core.Runtime;
+using UnityEngine;
+
+namespace UFlow.Addon.NetSync.Core.Runtime {
+    internal static class RpcRegistration {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void InitializeOnLoad() {
+            var subscriptionCacheType = typeof(RpcMethodSubscriptionCache<>);
+            var delegateType = typeof(RpcMethodDelegate<>);
+            var registerMethod = typeof(RpcRegistration).GetMethod(nameof(RegisterDelegate), BindingFlags.NonPublic | BindingFlags.Static);
+            foreach (var (method, att) in UFlowUtils.Reflection.GetAllMethodsWithAttribute<RpcMethodAttribute>(
+                UFlowUtils.Reflection.CommonExclusionNamespaces)) {
+                var delegateGenericType = delegateType.MakeGenericType(att.rpcType);
+                registerMethod!.MakeGenericMethod(att.rpcType)
+                    .Invoke(null, new object[] { subscriptionCacheType, delegateGenericType, method, att.sendType });
+            }
+        }
+
+        private static void RegisterDelegate<T>(in Type subscriptionCacheType, in Type delegateGenericType, 
+                                                in MethodInfo methodInfo, in RpcSendType sendType) where T : INetRpc {
+            subscriptionCacheType.MakeGenericType(typeof(T)).GetMethod(nameof(RpcMethodSubscriptionCache<T>.RegisterMethod))!.Invoke(null, 
+                new object[] { Delegate.CreateDelegate(delegateGenericType, methodInfo) as RpcMethodDelegate<T>, sendType });
+        }
+    }
+}
