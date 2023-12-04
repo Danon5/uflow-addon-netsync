64dcc29e013fc00e291e56dc1f5f29de35cf2528
Danon5 <danonfive@gmail.com>
2023-12-03 23:08:59 -0800

    added custom error when trying to spawn NetSceneEntity when network is not initialized

--- Core/Code/Runtime/NetSceneEntity.cs
+++ Core/Code/Runtime/NetSceneEntity.cs
@@ -1,10 +1,22 @@
 ﻿using UFlow.Addon.ECS.Core.Runtime;
+using UFlow.Core.Runtime;
 
 namespace UFlow.Addon.NetSync.Core.Runtime {
     public sealed class NetSceneEntity : SceneEntity {
         public override World GetWorld() => EcsModule<NetWorld>.Get().World;
 
-        protected override void Awake() => Initialize(NetSyncAPI.IsServer);
+        protected override void Awake() {
+            if (!NetSyncAPI.NetworkInitialized) {
+#if UFLOW_DEBUG_ENABLED
+                DebugAPI.LogError($"Attempting to instantiate NetSceneEntity when network is not initialized. Name: {name}");
+#endif
+                Destroy(gameObject);
+                return;
+            }
+            Initialize(NetSyncAPI.IsServer);
+        }
+
+        public override Entity CreateEntity() => NetSyncAPI.NetworkInitialized ? base.CreateEntity() : default;
 
         protected override void AddSpecialComponentsBeforeBaking() {
             base.AddSpecialComponentsBeforeBaking();
